3808a5365188c8699cd95375cb5e34b29d4af7e2
eunjeeing <ejjeongggg@gmail.com>
2024-08-31 17:02:00 +0900

    :memo: Comment: 주석 추가

--- app/src/main/java/com/ite/sws/common/RetrofitClient.kt
+++ app/src/main/java/com/ite/sws/common/RetrofitClient.kt
@@ -5,6 +5,19 @@ import com.google.gson.GsonBuilder
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+/**
+ * Retrofit Client
+ * @author 정은지
+ * @since 2024.08.31
+ * @version 1.0
+ *
+ * <pre>
+ * 수정일        	수정자        수정내용
+ * ----------  --------    ---------------------------
+ * 2024.08.31  	정은지       최초 생성
+ * </pre>
+ */
+
 object RetrofitClient {
     private const val BASE_URL = BuildConfig.BASE_URL
 
--- app/src/main/java/com/ite/sws/common/data/ErrorRes.kt
+++ app/src/main/java/com/ite/sws/common/data/ErrorRes.kt
@@ -1,9 +1,20 @@
 package com.ite.sws.common.data
 
+/**
+ * API 에러 응답 모델
+ * @author 정은지
+ * @since 2024.08.31
+ * @version 1.0
+ *
+ * <pre>
+ * 수정일        	수정자        수정내용
+ * ----------  --------    ---------------------------
+ * 2024.08.31  	정은지       최초 생성
+ * </pre>
+ */
+
 data class ErrorRes(
     val status: Int,
     val errorCode: String,
     val message: String
 )
-
-class ErrorResponseException(val errorRes: ErrorRes) : Throwable(errorRes.message)
