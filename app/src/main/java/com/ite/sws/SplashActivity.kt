6d876e3a53fc577d4e0b7ed5f8e57690d087b811
eunjeeing <ejjeongggg@gmail.com>
2024-09-16 00:26:54 +0900

    :memo: Comment: 주석 추가

--- app/src/main/java/com/ite/sws/SplashActivity.kt
+++ app/src/main/java/com/ite/sws/SplashActivity.kt
@@ -6,6 +6,18 @@ import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import com.ite.sws.databinding.ActivitySplashBinding
 
+/**
+ * 스플래시 액티비티
+ * @author 정은지
+ * @since 2024.09.13
+ * @version 1.0
+ *
+ * <pre>
+ * 수정일        	수정자        수정내용
+ * ----------  --------    ---------------------------
+ * 2024.09.13  	정은지       최초 생성
+ * </pre>
+ */
 class SplashActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashBinding
